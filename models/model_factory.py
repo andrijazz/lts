1192be53befe5cd28ae7e6b8b630cb582a9a21fe
andrijazz <a.djurisic@yahoo.com>
2024-09-03 12:38:44 +0200

    minor fix

--- models/model_factory.py
+++ models/model_factory.py
@@ -3,7 +3,6 @@ import torch
 from models.densenet import DenseNet3
 from models.mobilenetv2 import MobileNetV2
 from models.resnet_imagenet import ResNet50, ResNet101
-from models.vgg16 import vgg16
 from models.convnext import convnext_base
 from models.vit import vit_b_16, vit_b_32, vit_l_16, vit_l_32
 from models.swin_transformer import swin_b, swin_s
@@ -46,15 +45,6 @@ def build_model(model_name, num_classes):
             transforms.ToTensor(),
             transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
             # transforms.Normalize((125.3 / 255, 123.0 / 255, 113.9 / 255), (63.0 / 255, 62.1 / 255.0, 66.7 / 255.0)),
-        ])
-        return model, transform
-    if model_name == 'vgg16':
-        model = vgg16(pretrained=True)
-        transform = transforms.Compose([
-            transforms.Resize(256),
-            transforms.CenterCrop(224),
-            transforms.ToTensor(),
-            transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
         ])
         return model, transform
     if model_name == 'convnext':
